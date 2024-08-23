fc2dca32fda7e90e13fc1f0daf8c216f6f81fe33
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-08-23 10:04:15 +0300

    Added application DbContext

---
+++ Secure ASP.NET Core API with JWT Authentication/MyAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace MyAPI.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<IdentityUser>(options)
+{
+}
