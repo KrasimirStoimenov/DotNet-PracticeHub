de479fa6458417ea346612802e576a48785bf445
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-08-23 10:46:08 +0300

    Added JWT token generator service

---
+++ Secure ASP.NET Core API with JWT Authentication/MyAPI/Services/JWTTokenGenerator/IJWTTokenGenerator.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAPI.Services.JWTTokenGenerator;
+
+using Microsoft.AspNetCore.Identity;
+
+public interface IJWTTokenGenerator
+{
+    public string GenerateJWTToken(IdentityUser user);
+}
---
+++ Secure ASP.NET Core API with JWT Authentication/MyAPI/Services/JWTTokenGenerator/JWTTokenGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace MyAPI.Services.JWTTokenGenerator;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+public sealed class JWTTokenGenerator(IConfiguration configuration) : IJWTTokenGenerator
+{
+    public string GenerateJWTToken(IdentityUser user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["JWT:Issuer"],
+            audience: configuration["JWT:Issuer"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
