c1706b1a4c4687e5c522eb1562c6601839adafc4
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-08-26 13:58:53 +0300

    3. Introducing a Presentation Layer

---
+++ Domain Driven Design Guidebook/CarRentalSystem/CarRentalSystem.Web/Features/CarAdsController.cs
@@ -0,0 +1,19 @@
+﻿namespace CarRentalSystem.Web.Features;
+
+using CarRentalSystem.Domain.Models.CarAds;
+using CarRentalSystem.Domain.Models.Dealers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CarAdsController : ControllerBase
+{
+    private static readonly Dealer dealer = new("Dealer", "+12345678");
+
+    [HttpGet]
+    public IEnumerable<CarAd> Get()
+        => dealer
+            .CarAds
+            .Where(x => x.IsAvailable);
+}
