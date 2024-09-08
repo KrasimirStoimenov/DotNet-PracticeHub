021e2216d77e180108339d1affa0977c819bec11
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-09-08 14:31:20 +0300

    7. Creating Entities with Builder Factories - Unit tests

---
+++ Domain Driven Design Guidebook/CarRentalSystem/CarRentalSystem.Domain/Factories/Dealers/DealerFactory.Specs.cs
@@ -0,0 +1,37 @@
+﻿namespace CarRentalSystem.Domain.Factories.Dealers;
+
+using FluentAssertions;
+
+using Xunit;
+
+public class DealerFactorySpecs
+{
+    [Fact]
+    public void Build_ShouldCreateDealerWhenFluentSyntaxIsUsed()
+    {
+        // Arrange
+        var dealerFactory = new DealerFactory();
+
+        // Act
+        var dealer = dealerFactory
+            .WithName("TestDealer")
+            .WithPhoneNumber("+12345678")
+            .Build();
+
+        // Assert
+        dealer.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Build_ShouldCreateDealerWhenInlineParametersArePassed()
+    {
+        // Arrange
+        var dealerFactory = new DealerFactory();
+
+        // Act
+        var dealer = dealerFactory.Build("Valid dealer", "+12345678");
+
+        // Assert
+        dealer.Should().NotBeNull();
+    }
+}
